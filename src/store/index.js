1a9c54a069207b641eecc1354ad9767a2901c34a
SJ <sj@sj.com>
2021-12-01 08:28:11 +0100

    api url

--- src/store/index.js
+++ src/store/index.js
@@ -6,7 +6,7 @@ Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
-    API_URL: "http://localhost:5000",
+    API_URL: "http://serverexamensarbete.herokuapp.com",
     word: null,
     wordInfo: null,
     wordAuthor: null,
